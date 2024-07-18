c4d032192f0bba2eb577aa58d6a19a8e577eceaf
Loren Phillips <lorenphillips@protonmail.com>
2024-07-18 13:55:17 -0700

    Second rendition of optimization code

--- matlab/optimization/optimize_curves.m
+++ matlab/optimization/optimize_curves.m
@@ -1,44 +1,52 @@
 function optimize_curves(n, Theta, S_min, S_max, kappa_min, kappa_max)
-    % Define initial guess for segment lengths and curvatures
+    % Initial guess for segment lengths and curvatures
     S0 = linspace(S_min/n, S_max/n, n);
     kappa0 = linspace(kappa_min, kappa_max, n);
 
-    % Define the optimization problem
+    % Optimization problem defined
     options = optimoptions('fmincon', 'Display', 'iter', 'Algorithm', 'sqp');
     x0 = [S0, kappa0]; % Initial guess
     lb = [S_min/n * ones(1, n), kappa_min * ones(1, n)]; % Lower bounds
     ub = [S_max/n * ones(1, n), kappa_max * ones(1, n)]; % Upper bounds
 
-    % Optimize
-    [x, fval] = fmincon(@(x) -objective(x, n), x0, [], [], [], [], lb, ub, @(x) constraints(x, n, Theta, S_min, S_max), options);
+    % Optimization function...
+    [x, fval] = fmincon(@(x) -objective(x, n, S_min, S_max), x0, [], [], [], [], lb, ub, @(x) constraints(x, n, Theta, S_min, S_max), options);
 
     % Extract optimized segment lengths and curvatures
     S_opt = x(1:n);
     kappa_opt = x(n+1:end);
 
-    % results
+    % Compute curvatures for minimum and maximum lengths
+    kappa_min_lengths = kappa_opt .* (S_min ./ sum(S_opt));
+    kappa_max_lengths = kappa_opt .* (S_max ./ sum(S_opt));
+
+    % Results
     fprintf('Optimized segment lengths: \n');
     disp(S_opt);
     fprintf('Optimized curvatures: \n');
     disp(kappa_opt);
+    fprintf('Curvatures for minimum lengths: \n');
+    disp(kappa_min_lengths);
+    fprintf('Curvatures for maximum lengths: \n');
+    disp(kappa_max_lengths);
     fprintf('Maximum range (X_max - X_min): %f\n', -fval);
 end
 
-function f = objective(x, n)
+function f = objective(x, n, S_min, S_max)
     S = x(1:n);
     kappa = x(n+1:end);
     X_max = sum((1 - cos(kappa .* S)) ./ kappa);
-    X_min = sum((1 - cos(kappa .* (S/sum(S) * min(S)))) ./ kappa);
-    f = X_max - X_min; % Objective fucntion (maximize X_max - X_min)
+    X_min = sum((1 - cos(kappa .* (S / sum(S) * S_min))) ./ kappa);
+    f = X_max - X_min; % Objective function (maximize X_max - X_min)
 end
 
 function [c, ceq] = constraints(x, n, Theta, S_min, S_max)
     S = x(1:n);
     kappa = x(n+1:end);
     
-    % [Nonlinear equality constraints]
+    % Nonlinear equality constraints
     ceq = sum(kappa .* S) - Theta; % Maintain viewing angle
     
-    % [Nonlinear inequality constraints]
-    c = []; % none in this case
+    % Nonlinear inequality constraints
+    c = []; % None in this case
 end
